987c0da16a8f41a508df8400f9f00847c209d6ba
piotronm <123025664+piotronm@users.noreply.github.com>
2025-07-02 15:13:20 -0400

    Delete src directory

--- src/api/tags.ts
+++
@@ -1,8 +0,0 @@
-// src/api/tags.ts
-import api from "./axiosInstance";
-import type { Tag } from "../types/Tag";
-
-export async function fetchTags(): Promise<Tag[]> {
-  const response = await api.get("/Tag");
-  return response.data;
-}
--- src/api/users.ts
+++
@@ -1,7 +0,0 @@
-// src/api/users.ts
-import axios from "./axiosInstance";
-
-export async function checkIfAdmin(): Promise<boolean> {
-  const response = await axios.get("/users/check-admin");
-  return response.data.isAdmin === true;
-}
