a6e7f252000c987fa773c626216862e8bce49d0e
piotronm <123025664+piotronm@users.noreply.github.com>
2025-07-06 22:26:45 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import AppRoutes from './routes/AppRoutes';
+import NavBar from './components/NavBar';
+import { CssBaseline, Container, ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#f9fafb', // Matches Tailwind’s gray-50
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar />
+      <Container maxWidth="md">
+        <AppRoutes />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
