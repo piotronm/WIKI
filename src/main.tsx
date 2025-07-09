e24f58f70710530051c4ddd37bb72d2cc7d348a0
piotronm <123025664+piotronm@users.noreply.github.com>
2025-07-09 13:16:44 -0400

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ArticlesProvider } from "./context/ArticlesContext";
+import { AuthProvider } from "./context/AuthContext";
+import { TagsProvider } from "./context/TagsContext";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import theme from "./theme";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AuthProvider>
+          <TagsProvider>
+            <ArticlesProvider>
+              <App />
+            </ArticlesProvider>
+          </TagsProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
