4d4afd023f7d8fc8a34a41469d0019ee770b332f
piotronm <123025664+piotronm@users.noreply.github.com>
2025-07-11 14:30:13 -0400

    Add files via upload

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,31 @@
+// routes/AppRoutes.tsx
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Article from "../pages/Article";
+import Login from "../pages/Login";
+import AdminPanel from "../pages/AdminPanel";
+import UnauthorizedPage from "../pages/UnauthorizedPage";
+import NotFoundPage from "../pages/NotFoundPage";
+import SearchPage from "../pages/SearchPage";
+import ProtectedRoute from "../components/ProtectedRoute";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/article/:id" element={<Article />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute>
+            <AdminPanel />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/unauthorized" element={<UnauthorizedPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
