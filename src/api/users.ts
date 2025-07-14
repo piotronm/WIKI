d2228c09d73c336ff363c4fc125e216840ebe9e8
piotronm <123025664+piotronm@users.noreply.github.com>
2025-07-13 21:36:02 -0400

    Delete src directory

--- src/api/users.ts
+++
@@ -1,7 +0,0 @@
-// src/api/users.ts
-import axios from "./axiosInstance";
-
-export async function checkIfAdmin(userId: string): Promise<boolean> {
-  const res = await axios.get(`/User/${userId}`);
-  return res.data?.isAdmin === true;
-}
