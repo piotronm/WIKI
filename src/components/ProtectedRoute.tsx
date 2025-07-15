21c19df7e3e92fd92e72d685fefeee38d69ccda7
piotronm <123025664+piotronm@users.noreply.github.com>
2025-07-15 16:04:58 -0400

    Delete src directory

--- src/components/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { useAuth } from "../context/AuthContext";
-import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
-  const { user } = useAuth();
-
-  if (!user) return <Navigate to="/unauthorized" />;
-  return user.role === "admin" ? <>{children}</> : <Navigate to="/unauthorized" />;
-}
