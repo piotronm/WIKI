3f57abf99c8766b05f8ac431f6e16be400ccd30b
piotronm <123025664+piotronm@users.noreply.github.com>
2025-07-22 14:35:39 -0400

    Add files via upload

---
+++ src/utils/stripHtml.ts
@@ -0,0 +1,7 @@
+//src/utils/stripHtml.ts
+export function stripHtml(html: string): string {
+    const div = document.createElement("div");
+    div.innerHTML = html;
+    return div.textContent || div.innerText || "";
+  }
+  
